3267a769802c01ca4f1c9d51c7342e9ebe575632
devtonero <chidera.eagle@gmail.com>
2021-11-25 22:46:25 +0100

    button responsiveness

--- egbe/src/components/menu-item/menu-item.styles.jsx
+++ egbe/src/components/menu-item/menu-item.styles.jsx
@@ -52,6 +52,11 @@ export const ContentContainer = styled.div`
   background-color: white;
   opacity: 0.7;
   position: absolute;
+
+  @media screen and (max-width: 800px) {
+    padding: 0 15px;
+
+  }
   
    
 `;
