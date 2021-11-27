c53dcdd998627877218978917a24ff85d4c1e8c4
devtonero <chidera.eagle@gmail.com>
2021-11-27 22:12:39 +0100

    continue with our mobile responsiveness

--- egbe/src/components/collection-item/collection.components.jsx
+++ egbe/src/components/collection-item/collection.components.jsx
@@ -4,32 +4,34 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { addItem } from '../../redux/cart/cart.actions';
 
-import CustomButton from '../custom-buttom/custom-button.components';
 
-import './collection-item.styles.scss';
+
+
+import {
+  CollectionItemContainer,
+  CollectionFooterContainer,
+  AddButton,
+  BackgroundImage,
+  NameContainer,
+  PriceContainer
+} from './collection-item.styles';
 
 const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;
 
-  return (
-    <div className='collection-item'>
-      <div
-        className='image'
-        style={{
-          backgroundImage: `url(${imageUrl})`
-        }}
-      />
-      <div className='collection-footer'>
-        <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
-      </div>
-      <div className='custom-button '>
-      <CustomButton onClick={() => addItem(item)} inverted>
-        Add to cart
-      </CustomButton>
-      </div>
-    </div>
-  );
+  
+return (
+  <CollectionItemContainer>
+    <BackgroundImage className='image' imageUrl={imageUrl} />
+    <CollectionFooterContainer>
+      <NameContainer>{name}</NameContainer>
+      <PriceContainer>${price}</PriceContainer>
+    </CollectionFooterContainer>
+    <AddButton onClick={() => addItem(item)} inverted>
+      Add to cart
+    </AddButton>
+  </CollectionItemContainer>
+);
 };
 
 
